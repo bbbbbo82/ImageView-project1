082f9968c001e5eae454d6253f75ec4a660a0cc0
방문 사용자 <Guest@d7703-29ui-imac.local>
2019-03-28 15:32:03 +0900

    pro complete

--- TimerImageView02/ViewController.swift
+++ TimerImageView02/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImageViewer: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageViewer.image = UIImage(named: "frame1.png")
+        countLabel.text = "Frame\(count).png"
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        count += 1
+        
+        if(count>5){
+            count = 1
+        }
+        alienImageViewer.image = UIImage(named: "frame\(count).png")
+        countLabel.text = "frame\(count).png"
+        
+    }
 }
 
